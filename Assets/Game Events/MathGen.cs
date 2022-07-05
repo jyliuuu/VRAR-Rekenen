481b5426f90a78df561e94e0445b15570ab0b19a
Jacky Liu <00304501@rijnijssel.nl>
2022-07-05 09:36:07 +0200

    final commit

--- Assets/Game Events/MathGen.cs
+++ Assets/Game Events/MathGen.cs
@@ -25,7 +25,8 @@ public class MathGen : MonoBehaviour
     public bool answer2_bool = false;
     public bool answer3_bool = false;
 
-    public bool correct = false;
+    //setted bools alongside with their values
+    [SerializeField] private bool correct = false;
 
     ResultGen resultgen; //1st name is the script name & the 2nd one is the defined name
     public TextMeshPro display;
@@ -35,17 +36,18 @@ public class MathGen : MonoBehaviour
         QuestionGenerator();
 
         //get script
-        resultgen = display.GetComponent<ResultGen>();
-
-        //resultgen.correct = true;
+        //resultgen = display.GetComponent<ResultGen>();
     }
 
     private void QuestionGenerator()
     {
         {
-            string[] symbol = new string[] { "+", "-", //"x", "/" 
+            string[] symbol = new string[] { "+", "-"//, "x", "/" 
         };
+
             string randomSymbol = symbol[Random.Range(0, symbol.Length)];
+
+            Debug.Log(randomSymbol);
 
             if (randomSymbol == "+")
             {
@@ -53,7 +55,7 @@ public class MathGen : MonoBehaviour
                 int secondNumber = Random.Range(1, 101);
                 Question.text = firstNumber.ToString() + "  +  " + secondNumber.ToString() + "  =  ?";
                 int result = firstNumber + secondNumber;
-                int random = Random.Range(0, 3);
+                int random = Random.Range(1, 3);
 
                 int first = result - 5;
                 int second = result + 5;
@@ -105,13 +107,13 @@ public class MathGen : MonoBehaviour
                 }
 
             }
-            if (randomSymbol == "-")
+            else if (randomSymbol == "-")
             {
                 int firstNumber = Random.Range(1, 101);
                 int secondNumber = Random.Range(1, 101);
                 Question.text = firstNumber.ToString() + "  -  " + secondNumber.ToString() + "  =  ?";
                 int result = firstNumber - secondNumber;
-                int random = Random.Range(0, 3);
+                int random = Random.Range(1, 3);
                 int first = result - 5;
                 int second = result + 5;
 
@@ -159,54 +161,122 @@ public class MathGen : MonoBehaviour
 
                     SubmitChecker(randomint, randomint2, result, result);
                 }
-
-                // Answer1.text  = rnd.Next(0, 100);
-                // Answer2.text  = rnd.Next(0, 100);
-                // while (Answer2.text == Answer1.text)
-                //     {
-                //     Answer2.text = rnd.Next(0, 100);
-                //     }
             }
+            //else if (randomSymbol == "x")
+            //{
+            //    int firstNumber = Random.Range(1, 101);
+            //    int secondNumber = Random.Range(1, 101);
+            //    Question.text = firstNumber.ToString() + "  x  " + secondNumber.ToString() + "  =  ?";
+            //    int result = firstNumber * secondNumber;
+            //    int random = Random.Range(1, 3);
+            //    int first = result - 5;
+            //    int second = result + 5;
 
-            //if (randomSymbol == "x"){
-            //int firstNumber = Random.Range(1, 11);
-            //int secondNumber = Random.Range(1,101);
-            //text.text = firstNumber.ToString() + "  x  " + secondNumber.ToString() + "  =  ?";
-            //int result = firstNumber * secondNumber;
-            //Debug.Log(result);
+            //    if (random == 1)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(result.ToString());
+            //        Answer2.SetText(randomint2.ToString());
+            //        Answer3.SetText(randomint.ToString());
 
-            // int text2  = rnd.Next(0, 200);
-            // int text3  = rnd.Next(0, 200);
-            // while text3 == text2:
-            //     int text3 = rnd.Next(0, 200)
+            //        while (randomint == result) ;
+            //        {
+            //            randomint = Random.Range(first, second);
+            //        }
+            //        SubmitChecker(result, randomint2, randomint, result);
+            //    }
+            //    else if (random == 2)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(randomint.ToString());
+            //        Answer2.SetText(result.ToString());
+            //        Answer3.SetText(randomint2.ToString());
+
+            //        while (randomint == result) ;
+            //        {
+            //            randomint = Random.Range(first, second);
+            //        }
+
+            //        SubmitChecker(randomint, result, randomint2, result);
+            //    }
+            //    else if (random == 3)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(randomint.ToString());
+            //        Answer2.SetText(randomint2.ToString());
+            //        Answer3.SetText(result.ToString());
+
+            //        while (randomint == result) ;
+            //        {
+            //            int randomint = Random.Range(first, second);
+            //        }
+
+            //        SubmitChecker(randomint, randomint2, result, result);
+            //    }
+            //}
+            //else if (randomSymbol == "/")
+            //{
+            //    int firstNumber = Random.Range(1, 101);
+            //    int secondNumber = Random.Range(1, 101);
+            //    Question.text = firstNumber.ToString() + "  /  " + secondNumber.ToString() + "  =  ?";
+            //    int result = firstNumber / secondNumber;
+            //    int random = Random.Range(1, 3);
+            //    int first = result - 5;
+            //    int second = result + 5;
+
+            //    if (random == 1)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(result.ToString());
+            //        Answer2.SetText(randomint2.ToString());
+            //        Answer3.SetText(randomint.ToString());
+
+            //        while (randomint == result) ;
+            //        {
+            //            randomint = Random.Range(first, second);
+            //        }
+            //        SubmitChecker(result, randomint2, randomint, result);
+            //    }
+            //    else if (random == 2)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(randomint.ToString());
+            //        Answer2.SetText(result.ToString());
+            //        Answer3.SetText(randomint2.ToString());
+
+            //        while (randomint == result) ;
+            //        {
+            //            randomint = Random.Range(first, second);
+            //        }
+
+            //        SubmitChecker(randomint, result, randomint2, result);
+            //    }
+            //    else if (random == 3)
+            //    {
+            //        randomint = Random.Range(first, second);
+            //        randomint2 = Random.Range(first, second);
+            //        Answer1.SetText(randomint.ToString());
+            //        Answer2.SetText(randomint2.ToString());
+            //        Answer3.SetText(result.ToString());
+
+            //        while (randomint == result) ;
+            //        {
+            //            int randomint = Random.Range(first, second);
+            //        }
+
+            //        SubmitChecker(randomint, randomint2, result, result);
+            //    }
+            //}
         }
-        //if (randomSymbol == "/"){
-        //int firstNumber = Random.Range(1, 101);
-        //int secondNumber = Random.Range(1,11);
-        //text.text = firstNumber.ToString() + "  /  " + secondNumber.ToString() + "  =  ?";
-        //int result = firstNumber / secondNumber;
-        //Debug.Log(result);
-
-        // int text2  = rnd.Next(0, 200);
-        // int text3  = rnd.Next(0, 200);
-        // while text3 == text2:
-        //     int text3 = rnd.Next(0, 200)
     }
 
     public void SubmitChecker(int a1, int a2, int a3, int answer)
     {
-        //if (a1 == true) //target1 geschoten
-        //{
-        //    this.correct = true;
-        //}
-        //else if (a2 == true) //target1 geschoten
-        //{
-        //    this.correct = true;
-        //}
-        //else if (a3 == true) //target1 geschoten
-        //{
-        //    this.correct = true;
-        //}
     }
 }
 //}
--- Assets/Game Events/ResultGen.cs
+++ Assets/Game Events/ResultGen.cs
@@ -24,6 +24,6 @@ public class ResultGen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(correct);
+        //Debug.Log(correct);
     }
 }
